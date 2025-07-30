6f837b54cf831567ca9f80ceca78cb5500fa529d
MohamedMenaisy <kaledibrahim989@gmail.com>
2025-07-30 13:48:45 +0300

    Add files via upload

---
+++ calculator2.py
@@ -0,0 +1,18 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operator = input("Enter an operator (+, -, *, /): ")
+if operator == '+':
+    result = num1 + num2
+    print("Result:", result)
+elif operator == '-':
+    result = num1 - num2
+    print("Result:", result)
+elif operator == '*':
+    result = num1 * num2
+    print("Result:", result)
+elif operator == '/':
+    if num2 != 0:
+        result = num1 / num2
+        print("Result:", result)
+
+ 
